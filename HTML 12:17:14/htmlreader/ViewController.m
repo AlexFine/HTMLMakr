46bdf25bd3f5c1ff7ccef84b19372735b3786b14
AlexFine <alexkfine2@gmail.com>
2015-09-27 09:24:10 -0700

    Implemented iAds

--- HTML 12:17:14/htmlreader/ViewController.m
+++ HTML 12:17:14/htmlreader/ViewController.m
@@ -20,6 +20,9 @@
 {
   //  BOOL _bannerIsVisible;
  //   ADBannerView *_adBanner;
+    
+    BOOL _bannerIsVisible;
+    ADBannerView *_adBanner;
 }
 
 @end
@@ -104,7 +107,9 @@ _Video2Back.layer.borderColor=[[UIColor darkGrayColor]CGColor];
   //  var num: Int;
 
     int sum = 0;
- /*
+   
+    
+    /*
 UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(slideToLeftWithGestureRecognizer:)];
    swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
     [self.view addGestureRecognizer:swipeLeft];
@@ -152,6 +157,9 @@ UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWith
     
   //  _adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, 152, 320, 50)];
 //    _adBanner.delegate = self;
+    
+    _adBanner = [[ADBannerView alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, 320, 50)];
+    _adBanner.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -333,7 +341,43 @@ int i=0;
     [textView resignFirstResponder];
     return YES;
 }
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner
+{
+    if (!_bannerIsVisible)
+    {
+        // If banner isn't part of view hierarchy, add it
+        if (_adBanner.superview == nil)
+        {
+            [self.view addSubview:_adBanner];
+        }
+        
+        [UIView beginAnimations:@"animateAdBannerOn" context:NULL];
+        
+        // Assumes the banner view is just off the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, -banner.frame.size.height);
+        
+        [UIView commitAnimations];
+        
+        _bannerIsVisible = YES;
+    }
+}
 
+- (void)bannerView:(ADBannerView *)banner didFailToReceiveAdWithError:(NSError *)error
+{
+    NSLog(@"Failed to retrieve ad");
+    
+    if (_bannerIsVisible)
+    {
+        [UIView beginAnimations:@"animateAdBannerOff" context:NULL];
+        
+        // Assumes the banner view is placed at the bottom of the screen.
+        banner.frame = CGRectOffset(banner.frame, 0, banner.frame.size.height);
+        
+        [UIView commitAnimations];
+        
+        _bannerIsVisible = NO;
+    }
+}
 
 //int values[4] = {240, 320, 640, 920};
 //int value = values[random() % 4];
